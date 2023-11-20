9d5324d831e86efe126056ee2f9f2a05129ae4cb
Saumya-Purohit <saumyanotes@gmail.com>
2023-11-20 17:04:47 +0530

    project done

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "appsyncblogapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "appsyncblogapp1ff85604": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "postimages": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
